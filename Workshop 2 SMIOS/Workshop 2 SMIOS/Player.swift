93c961a92097b97caccf50e80b310b218ec07c09
Bart van de Klundert <b.vdklundert@outlook.com>
2019-02-14 10:48:30 +0100

    Storyboard Tutorial part 1

---
+++ Workshop 2 SMIOS/Workshop 2 SMIOS/Player.swift
@@ -0,0 +1,17 @@
+//
+//  Player.swift
+//  Workshop 2 SMIOS
+//
+//  Created by Bart van de Klundert on 14/02/2019.
+//  Copyright © 2019 Bart van de Klundert. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    
+    // MARK: - Properties
+    var name: String?
+    var game: String?
+    var rating: Int
+}
---
+++ Workshop 2 SMIOS/Workshop 2 SMIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Workshop 2 SMIOS
+//
+//  Created by Bart van de Klundert on 14/02/2019.
+//  Copyright © 2019 Bart van de Klundert.rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
